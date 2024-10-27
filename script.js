b195e7874b2cddc0215dbe445e32d7db2e45efcb
mickey-40 <mickeyarnold53@yahoo.com>
2024-10-27 16:31:50 -0500

    added a loading screen

--- script.js
+++ script.js
@@ -3,22 +3,40 @@ const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
 
 let apiQuotes = [];
 
+// show loading
+const loading = () => {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// hide loading
+const complete = () => {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // New Quote
 const newQuote = () =>{
+  loading();
   // Pick random quote from api array
   const quote = apiQuotes[Math.floor(Math.random()* apiQuotes.length)];
   // Check if author field is blank or Anonymous and replace with 'Unknown
   let author = !quote.author || quote.author === 'Anonymous' ? authorText.textContent = 'Unknown': authorText.textContent = quote.author;
   // Check quote length to determine styling
   let quoteStyle = quote.text.length > 120 ? quoteText.classList.add('long-quote'): quoteText.classList.remove('long-quote');
+
+  // set quote, hide loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes from API
 const getQuotes = async () => {
+  loading();
   const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json'
   try{
     const response = await fetch(apiUrl);
@@ -41,4 +59,4 @@ newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
-getQuotes()
+getQuotes();
