2453fb90756ebc6957622c7f6faae4b8e9332188
mickey-40 <mickeyarnold53@yahoo.com>
2024-10-27 16:24:18 -0500

    finished javascript-can pull from api and post to twitter

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
 let apiQuotes = [];
 
 // New Quote
 const newQuote = () =>{
   // Pick random quote from api array
   const quote = apiQuotes[Math.floor(Math.random()* apiQuotes.length)];
-  console.log(quote)
+  // Check if author field is blank or Anonymous and replace with 'Unknown
+  let author = !quote.author || quote.author === 'Anonymous' ? authorText.textContent = 'Unknown': authorText.textContent = quote.author;
+  // Check quote length to determine styling
+  let quoteStyle = quote.text.length > 120 ? quoteText.classList.add('long-quote'): quoteText.classList.remove('long-quote');
+  quoteText.textContent = quote.text;
 }
 
 // Get Quotes from API
@@ -19,6 +29,16 @@ const getQuotes = async () => {
     // Catch Error Here
   }
 }
+
+// Tweet a quote
+const tweetQuote = () => {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes()
